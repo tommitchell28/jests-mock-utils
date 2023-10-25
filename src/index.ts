3a07e27e938051b0da2ee47a0a5e0e5c1b67634d
tommitchell28 <tomjmitchell28@gmail.com>
2023-10-25 21:32:25 +0100

    feat: export from index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { createTypedMockClass, getTypedMockInstances } from './mocks/class';
+export { createMockedClassInstance } from './mocks/classInstance';
+export { createMockFromType } from './mocks/fromType';
+export {
+    createStrictTypedMockFunction,
+    createPartialTypedMockFunction,
+    createDeepPartialTypedMockFunction,
+} from './mocks/function';
