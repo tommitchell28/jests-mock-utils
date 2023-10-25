2c5f7bdb6543b8a2e9340cbdeb48d90177a33083
tommitchell28 <tomjmitchell28@gmail.com>
2023-10-25 21:03:06 +0100

    feat: add partials types file

---
+++ src/types/partials.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+type DeepPartial<T> = {
+    [P in keyof T]?: T[P] extends (infer U)[]
+        ? DeepPartial<U>[]
+        : T[P] extends readonly (infer U)[]
+        ? readonly DeepPartial<U>[]
+        : T[P] extends object
+        ? DeepPartial<T[P]>
+        : T[P];
+};
+
+export type PartialReturnType<T extends (...args: any[]) => any> = T extends (
+    ...args: any[]
+) => infer R
+    ? Partial<R>
+    : never;
+
+export type DeepPartialReturnType<T extends (...args: any[]) => any> = T extends (
+    ...args: any[]
+) => infer R
+    ? DeepPartial<R>
+    : never;
