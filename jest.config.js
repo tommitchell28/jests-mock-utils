9838bf07460e23e351e0e3b3f8cb067f787c3871
tommitchell28 <tomjmitchell28@gmail.com>
2023-10-26 23:52:58 +0100

    test: update jest config to collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testMatch: ['<rootDir>/tests/**/*test.ts'],
 };
